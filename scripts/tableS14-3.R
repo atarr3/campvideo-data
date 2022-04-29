907ff53b233b9a15e0a19c628d1649cd0464d785
Alex Tarr <atarr3@gmail.com>
2022-04-29 04:55:02 -0400

    LDA fixes and figure name changes

--- scripts/tableS14-3.R
+++ scripts/tableS14-3.R
@@ -40,7 +40,7 @@ LSD <- LSD.all[LSD.all$uid %in% tone.wmp.sub$uid, "tone"]
 WMP <- as.integer(tone.wmp.sub$tonecmag %in% c("POSITIVE", "POS"))
 
 # confusion matrix (full sample)
-cm.full <- format(round(table(LSD, WMP) / length(LSD) * 100, 2), nsmall=2)
+cm.full <- format(round(table(WMP, LSD) / length(LSD) * 100, 2), nsmall=2)
 
 # read in predictions
 tone.pred <- read.csv(here::here("results", "negativity_results.csv"), 
@@ -51,7 +51,13 @@ tone.pred <- tone.pred[tone.pred$feature == "text" & tone.pred$model == "nsvm" &
                        tone.pred$train == 0, c("uid", "tone")]
 
 # confusion matrix (test set)
-cm.test <- format(round(table(LSD, WMP) / length(LSD) * 100, 2), nsmall=2)
+LSD <- LSD.all[match(tone.pred$uid, LSD.all$uid), "tone"]
+WMP <- WMP[match(tone.pred$uid, tone.wmp.sub$uid)]
+cm.test <- format(round(table(WMP, LSD) / length(LSD) * 100, 2), nsmall=2)
+
+# confusion matrix (our)
+AUTO <- tone.pred$tone
+cm.ours <- format(round(table(WMP, AUTO) / length(LSD) * 100, 2), nsmall=2)
 
 # save
 cat("LSD Results (Full)\n", file=here::here("tables", "tableS14-3.txt"))
@@ -66,3 +72,8 @@ sink(file=here::here("tables", "tableS14-3.txt"), append=T)
 print(cm.test)
 sink(file=NULL)
 cat("\n", file=here::here("tables", "tableS14-3.txt"), append=T)
+cat("LSD Results (Ours)\n", file=here::here("tables", "tableS14-3.txt"), append=T)
+cat("------------------\n", file=here::here("tables", "tableS14-3.txt"), append=T)
+sink(file=here::here("tables", "tableS14-3.txt"), append=T)
+print(cm.ours)
+sink(file=NULL)
